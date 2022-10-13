594479eeecaf8fbf9ff61ba16dbf911673a0619f
VictorKreed <okubulevictor17@gmail.com>
2022-10-13 04:05:14 -0700

    new file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,5 @@
 #include <stdio.h>
-
-
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main()
-
-{
-	char str[]="\"Programming is like building a multilingual puzzle";
-	puts(str);  
-
-	    return (0);
-
-}
+int main(int argc, char *argv[])
+{char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+return (0); }
