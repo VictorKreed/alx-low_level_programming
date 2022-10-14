487a63657cd91c6b2202e55c71b6226e68b1ebdd
VictorKreed <okubulevictor17.gmail.com>
2022-10-14 12:42:08 -0700

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int var = 48;
+char tyo = '\n';
+while (var < 68)
+{
+putchar(var);
+var++;
+}
+putchar(tyo);
+return (0);
+}
