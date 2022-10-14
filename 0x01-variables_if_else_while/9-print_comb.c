b8df4b21dfc473e9e7c90a4ac82f04c089adf021
VictorKreed <okubulevictor17.gmail.com>
2022-10-14 13:30:23 -0700

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int var = 48;
+int gar = 44;
+int far = 32;
+while (var < 58)
+{
+putchar(var);
+if (var < 57)
+{
+putchar(gar);
+putchar(far);
+}
+var++;
+}
+return (0);
+}
