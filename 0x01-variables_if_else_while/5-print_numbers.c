8420448ca140260e50ea44d6277e366c704113c8
VictorKreed <okubulevictor17.gmail.com>
2022-10-14 11:11:09 -0700

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int var = 0;
+while (var < 10)
+{
+printf("%d\n", var);
+var++;
+}
+return (0);
+}
