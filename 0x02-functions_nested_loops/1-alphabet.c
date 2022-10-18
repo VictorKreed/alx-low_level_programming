ab0300ac3df6127d7e5eebeb726476beacb89c25
VictorKreed <okubulevictor17.gmail.com>
2022-10-18 05:01:24 -0700

    new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+int ally = 97;
+int pull = 122;
+while (ally <= pull)
+{
+putchar(ally);
+ally++;
+}
+return (0);
+}
+int main(void)
+{
+int mor = '\n';
+print_alphabet();
+putchar(mor);
+return (0);
+}
